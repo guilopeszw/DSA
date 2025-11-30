e28bd323e1f5ee6ba89a0d193e2a90090f94cf2a
Guilherme Lopes <guilherme.lopes3@academico.ufpb.br>
2025-11-30 08:57:29 -0300

    fix: invert_vector.cpp

--- c_studies/homework/book/invert_vector.cpp
+++ c_studies/homework/book/invert_vector.cpp
@@ -1,4 +1,40 @@
-// [ 1,  4,  7,  9 ]
-// [ 9,  7,  4,  1 ]
+#include <iostream>
+using namespace std;
 
-void inverte(int n, int* v)
+// {2, 4, 3, 9, 1}
+//        j
+// {1, 9, 3, 4, 2}
+//        i
+
+
+int* inverte(int n, int* v) {
+    // int* inverso = (int*) malloc(n * sizeof(int));
+    int* inverso = new int[n];
+    int j = n - 1;
+
+    for (int i = 0; i < n; i++) {
+        inverso[i] = v[j];
+        j -= 1;
+    }
+    
+    return inverso;
+}
+
+int main() {
+    int v[5] = {2, 4, 3, 9, 1};
+    int arraySize = sizeof(v) / sizeof(v[0]);
+
+    cout << "vetor original: \n";
+    for (int i = 0; i < arraySize; i++) {
+        cout << v[i];
+    }
+    cout << endl;
+
+    cout << "vetor invertido: \n";
+    int* invertido = inverte(arraySize, v);
+    for (int i = 0; i < arraySize; i++) {
+        cout << invertido[i];
+    }
+
+    free(invertido);
+}
