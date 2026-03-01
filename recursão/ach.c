a4d2effd57c999d2d4c3b8695c44679de9bec8f2
Guilherme Lopes <guilherme.lopes3@academico.ufpb.br>
2026-03-01 15:32:55 -0300

    fix: folder names

--- recursão/ach.c
+++
@@ -1,5 +0,0 @@
-unsigned int acherman(unsigned int n, unsigned int m) {
-    if (m == 0) return (n + 1);
-    if (m > 0 && n == 0) return acherman(m -1, m);
-    if (m > 0 && n > 0) return acherman(m - 1, acherman(m, n - 1));
-}
--- recursão/cat.c
+++
@@ -1,5 +0,0 @@
-unsigned int cat(unsigned int n) {
-    if (n == 0) return 1;
-
-    return ((2 * (2 * n - 1)) * cat(n - 1))/(n + 1);
-}
